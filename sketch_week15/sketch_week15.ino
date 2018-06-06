5c5c4e5ed25f864fdddd1cf0832b487a07b10ff7
Bern0630 <39110140+Bern0630@users.noreply.github.com>
2018-06-06 11:29:36 +0800

    Add files via upload

---
+++ sketch_week15/sketch_week15.ino
@@ -0,0 +1,17 @@
+int outputValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorRead = analogRead(A0);
+if(sensorRead < 650)sensorRead = 650;
+if(sensorRead > 955)sensorRead = 955;
+int outputValue = map(sensorRead, 650, 955, 0, 255);
+analogWrite(9, outputValue);
+Serial.println(outputValue);
+delay(200);
+}
